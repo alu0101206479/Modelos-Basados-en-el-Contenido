32301e9e27669276bf97efdb4763a8db809f5cd2
ACOIDAN MESA HERNANDEZ <ALU0101206479@ULL.EDU.ES>
2021-12-28 15:33:57 +0000

    Práctica acabada (A falta de informe)

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Modelos-Basados-en-el-Contenido
-Repositorio para la práctica "Sistemas de recomendación. Modelos Basados en el Contenido" de la asignatura Gestión del Conocimiento en las Organizaciones
+# Sistemas de recomendación. Modelos Basados en el Contenido
+## ACOIDAN MESA HERNANDEZ - [alu0101206479@ull.edu.es](alu0101206479@ull.edu.es)
+
+### Descripción del código desarrollado
+
+Se ha desarrollado un código en ```HTML``` y ```Javascript```, este código consta de un fichero HTML con un formulario con 1 inputs:
+
+  * Archivo de texto plano con extensión .txt. Cada documento viene representado en una línea del archivo.
+
+Este formulario consta de un botón que cuando se pulsa llama a la función ```tratar_datos()``` del código de Javascript.
+
+El fichero de Javascript consta de las siguientes funciones:
+
+  * ```tratar_datos()```: Función a la que se llama desde el html, llama a la función ```obtener_tablas(documentos)``` y ```obtener_similiaridades(informacion_documentos)``` y muestra las salidas correspondientes en el HTML.
+  * ```obtener_tablas(documentos)```: Función en la que se completa y se devuelve un vector con la información de los términos de los documentos (TF, IDF, TF-IDF, TF normalizados).
+  * ```obtener_similiaridades(informacion_documentos)```: Función con la que se obtiene las similiaridades coseno entre los pares de documentos.
+  * ```tf(terminos, documento)```: Función para calcular el TF de todos los términos de un documento determinado.
+  * ```idf(terminos, documentos)```: Función para calcular el IDF de todos los términos de un documento determinado.
+  * ```tf_idf(tf, idf)```: Función para calcular el TF-IDF de todos los términos de un documento determinado.
+
+A parte de estas funciones en el fichero de Javascript también hay un manejador para el evento ```change``` del input en el que se selecciona el fichero que contiene los documentos, para obtener los terminos de estos y almacenarlos en una variable.
+
+### Ejemplo de uso
+
+Parametros de entrada:
+  * Fichero: Seleccionar un fichero que contenga los siguientes documentos (Nos tenemos que asegurar que este fichero solo contenga espacios entre medio de los terminos, y no espacios ni al final ni al principio de cada fila o documento):  
+    ```
+    Aromas include tropical fruit, broom, brimstone and dried herb. The palate isn't overly expressive, offering unripened apple, citrus and dried sage alongside brisk acidity.
+    This is ripe and fruity, a wine that is smooth while still structured. Firm tannins are filled out with juicy red berry fruits and freshened with acidity. It's  already drinkable, although it will certainly be better from 2016.
+    Tart and snappy, the flavors of lime flesh and rind dominate. Some green pineapple pokes through, with crisp acidity underscoring the flavors. The wine was all stainless-steel fermented.
+    Pineapple rind, lemon pith and orange blossom start off the aromas. The palate is a bit more opulent, with notes of honey-drizzled guava and mango giving way to a slightly astringent, semidry finish.
+    Much like the regular bottling from 2012, this comes across as rather rough and tannic, with rustic, earthy, herbal characteristics. Nonetheless, if you think of it as a pleasantly unfussy country wine, it's a good companion to a hearty winter stew.
+    Blackberry and raspberry aromas show a typical Navarran whiff of green herbs and, in this case, horseradish. In the mouth, this is fairly full bodied, with tomatoey acidity. Spicy, herbal flavors complement dark plum fruit, while the finish is fresh but grabby.
+    Here's a bright, informal red that opens with aromas of candied berry, white pepper and savory herb that carry over to the palate. It's balanced with fresh acidity and soft tannins.
+    This dry and restrained wine offers spice in profusion. Balanced with acidity and a firm texture, it's very much for food.
+    Savory dried thyme notes accent sunnier flavors of preserved peach in this brisk, off-dry wine. It's fruity and fresh, with an elegant, sprightly footprint.
+    This has great depth of flavor with its fresh apple and pear fruits and touch of spice. It's off dry while balanced with acidity and a crisp texture. Drink now.
+    ```
+
+
+Salida:
+  
+[![Video Salida](./Ejemplos/Salidas/documents-01.mp4)](./Ejemplos/Salidas/documents-01.mp4)
--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 var fichero = document.getElementById('fichero')
 let documentos = []
 
-// Leemos el fichero y almacenamos en matriz_entrada la matriz que contiene
+// Leemos el fichero y almacenamos en documentos un vector en el que cada posicion contiene un vector con las palabras de cada documento respectivamente
 fichero.addEventListener('change', function(e) {
+  documentos = []
+
   let reader = new FileReader();
   reader.onload = function () {
     let lines = reader.result.toString()
@@ -14,26 +16,34 @@ fichero.addEventListener('change', function(e) {
       fila = fila.replace(/\./gi, '');
       fila = fila.toLowerCase();
       documentos.push(fila.split(" "));
-
-      //documentos[i] = documentos[i].filter((term, index) => documentos[i].indexOf(term) === index);
     })
   }
   reader.readAsText(fichero.files[0]);
 }, false)
 
 
-function tf_idf(tf, idf) {
-  let tf_idf = []
 
-  // TF y IDF son vectores del mismo tamaño
-  for (let i = 0; i < tf.length; i++) {
-    tf_idf.push(tf[i]*idf[i])
-  }
+// Función para calcular el TF de todos los términos de un documento determinado
+function tf(terminos, documento) {
+  let tfs = []
 
-  return tf_idf;
+  terminos.forEach((term) => {
+    let cont = 0;
+    documento.forEach((termD) => {
+      if (termD === term) {
+        cont++;
+      }
+    })
+    tfs.push(cont);
+  })
+
+  return tfs
 }
 
-function idf(documentos, terminos) {
+
+
+// Función para calcular el IDF de todos los términos de un documento determinado
+function idf(terminos, documentos) {
   let idf = [];
 
   terminos.forEach((termino) => {
@@ -44,7 +54,7 @@ function idf(documentos, terminos) {
       }
     });
 
-    idf.push(Math.log(documentos.length/ocurrencias)/Math.log(10));
+    idf.push(Math.log10(documentos.length/ocurrencias));
   })
 
   return idf;
@@ -52,135 +62,264 @@ function idf(documentos, terminos) {
 
 
 
+// Función para calcular el TF-IDF de todos los términos de un documento determinado
+function tf_idf(tf, idf) {
+  let tf_idf = []
 
-// Función a la que se llama desde el html, recoge los datos y muestra la salida
-function tratar_datos() {
-  let body = document.getElementById("salida");
+  // TF y IDF son vectores del mismo tamaño ya que refieren a los mismos términos
+  for (let i = 0; i < tf.length; i++) {
+    tf_idf.push(tf[i]*idf[i])
+  }
 
-  // Crea un elemento <table> y un elemento <tbody>
+  return tf_idf;
+}
 
 
+
+// Función en la que se completa y se devuelve un vector con la información de los términos de los documentos (TF, IDF, TF-IDF, TF normalizados)
+function obtener_tablas(documentos) {
+  let informacion_documentos = []     //  Vector en la que cada posición es un documento, y en cada posicion se almacena un vector con subvectores [indice, termino, tf, idf, tf-idf, tf-normalizados]
+
+  // Completamos un vector con la información de los términos de cada documento y lo pusheamos en el vector con la información de todos los documentos
   documentos.forEach((documento) => {
-    var tabla   = document.createElement("table");
-    tabla.classList.add('table');
-    tabla.classList.add('table-bordered');
-    tabla.classList.add('border-dark');
-    
-    let tblBody = document.createElement("tbody");
+    let informacion_documento = [];
 
     // Quitamos términos duplicados para obtener un vector de términos
-    let indice_terminos = JSON.parse(JSON.stringify(documento.filter((term, index) => documento.indexOf(term) === index)));
+    let terminos = JSON.parse(JSON.stringify(documento.filter((term, index) => documento.indexOf(term) === index)));
+
 
     // Índices y Términos
-    let fila_indices = document.createElement("tr");
-    let celda_indices = document.createElement("th");
-    let textoCelda_indices = document.createTextNode("Índices");
-    celda_indices.appendChild(textoCelda_indices);
-    fila_indices.appendChild(celda_indices)
-
-    let fila_terminos = document.createElement("tr");
-    let celda_terminos = document.createElement("th");
-    let textoCelda_terminos = document.createTextNode("Términos");
-    celda_terminos.appendChild(textoCelda_terminos);
-    fila_terminos.appendChild(celda_terminos)
-
-    indice_terminos.forEach((termino, index) => {
-      celda_terminos = document.createElement("td");
-      textoCelda_terminos = document.createTextNode(termino);
-      celda_terminos.appendChild(textoCelda_terminos);
-      fila_terminos.appendChild(celda_terminos)
-
-      celda_indices = document.createElement("td");
-      textoCelda_indices = document.createTextNode(index);
-      celda_indices.appendChild(textoCelda_indices);
-      fila_indices.appendChild(celda_indices)
+    terminos.forEach((termino, index) => {
+      informacion_documento.push([index, termino]);
     });
-
-
-    tblBody.appendChild(fila_indices)
-    tblBody.appendChild(fila_terminos)
-
 
     // TF
-    let tf = [];
+    let tfV = tf(terminos, documento);
 
-    indice_terminos.forEach((term) => {
-      let cont = 0;
-      documento.forEach((termD) => {
-        if (termD === term) {
-          cont++;
-        }
-      })
-      tf.push(cont);
+    tfV.forEach((frecuencia, index) => {
+      informacion_documento[index].push(frecuencia);
     })
-
-    let fila_tf = document.createElement("tr");
-    let celda_tf = document.createElement("th");
-    let textoCelda_tf = document.createTextNode("TF");
-    celda_tf.appendChild(textoCelda_tf);
-    fila_tf.appendChild(celda_tf)
-
-    tf.forEach((tfT, index) => {
-      celda = document.createElement("td");
-      let textoCelda = document.createTextNode(tfT);
-      celda.appendChild(textoCelda);
-      fila_tf.appendChild(celda)
-    });
-
-    tblBody.appendChild(fila_tf)
-
 
 
     // IDF
-
-    let idfV = idf(documentos, indice_terminos)
-
-    let fila_idf = document.createElement("tr");
-    let celda_idf = document.createElement("th");
-    let textoCelda_idf = document.createTextNode("IDF");
-    celda_idf.appendChild(textoCelda_idf);
-    fila_idf.appendChild(celda_idf)
+    let idfV = idf(terminos, documentos)
 
     idfV.forEach((idfT, index) => {
-      celda = document.createElement("td");
-      let textoCelda = document.createTextNode(idfT);
-      celda.appendChild(textoCelda);
-      fila_idf.appendChild(celda)
-    });
-
-    tblBody.appendChild(fila_idf)
-
-
-    tabla.appendChild(tblBody)
-    body.appendChild(tabla);
-  
-
-  
+      informacion_documento[index].push(idfT);
+    });  
   
     // TF-IDF
-    let tf_idfV = tf_idf(tf, idfV)
-    console.log(tf, idfV, tf_idfV);
-    let fila_tf_idf = document.createElement("tr");
-    let celda_tf_idf = document.createElement("th");
-    let textoCelda_tf_idf = document.createTextNode("TF-IDF");
-    celda_tf_idf.appendChild(textoCelda_tf_idf);
-    fila_tf_idf.appendChild(celda_tf_idf)
+    let tf_idfV = tf_idf(tfV, idfV)
 
     tf_idfV.forEach((tf_idfT, index) => {
-      celda = document.createElement("td");
-      let textoCelda = document.createTextNode(tf_idfT);
-      celda.appendChild(textoCelda);
-      fila_tf_idf.appendChild(celda)
+      informacion_documento[index].push(tf_idfT);
     });
 
-    tblBody.appendChild(fila_tf_idf)
 
+    // Principio de la similiaridad coseno (Normalización)
 
+    // Calculo del tf que se utiliza para la similiaridad 1+log(tf)
+    let tf_similiaridad = [];
+    tfV.forEach((frecuencia, index) => {
+      tf_similiaridad.push(1+Math.log10(frecuencia));
+    })
 
+    // Calculo de longitudes
+    let sumatorio = 0;
+    tf_similiaridad.forEach((frecuencia) => {
+      sumatorio = sumatorio + Math.pow(frecuencia, 2)
+    })
+    
+    let longitud = Math.sqrt(sumatorio)
 
-    tabla.appendChild(tblBody)
-    body.appendChild(tabla);
+    // Normalización
+    tf_similiaridad.forEach((frecuencia, index) => {
+      informacion_documento[index].push(frecuencia/longitud);
+    })
+
+    informacion_documentos.push(informacion_documento);
   })
 
-  
+
+  return informacion_documentos;
+}
+
+
+
+// Función con la que se obtiene las similiaridades coseno entre los pares de documentos
+function obtener_similiaridades(informacion_documentos) {
+  let similiaridades = []   // Vector que almacena las similaridades, este vector tiene el formato [[Documento1, [[Documento2, COS(D1,D2)], [Documento3, COS(D1,D3)]]], [Documento2, [[Documento1, COS(D2,D1)], [Documento3, COS(D2,D3)]]].....]
+
+  for (let i = 0; i < informacion_documentos.length; i++) {
+    for (let j = 0; j < informacion_documentos.length; j++) {
+      
+      if (i !== j) {  // Condicional para que no se haga comparación entre documentos iguales
+        let coseno = 0;
+        for (let k = 0; k < informacion_documentos[i].length; k++) {  // Se hace el sumatorio de las multiplicaciones de los TF normalizados de las palabras comunes entre ambos documentos
+          let indice = informacion_documentos[j].findIndex((palabra) => {
+            if (informacion_documentos[i][k][1] === palabra[1]) {
+              return true;
+            }
+          })
+
+          if (indice !== -1) {
+            coseno = coseno + (informacion_documentos[i][k][5] * informacion_documentos[j][indice][5])
+            console.log(informacion_documentos[i][k][5], informacion_documentos[j][indice][5], coseno);
+          }
+        }
+
+        let indice = similiaridades.findIndex((documento) => {
+          if (documento[0] === i) {
+            return true;
+          }
+        });
+
+        if (indice === -1) {
+          similiaridades.push([i, [[j, coseno]]])
+        } else {
+          similiaridades[indice][1].push([j, coseno]);
+        }
+      }
+    }
+  }
+
+  return similiaridades;
+}
+
+
+// Función a la que se llama desde el html, llama a la función obtener_tablas() y obtener_similiaridades(informacion_documentos) y muestra las salidas correspondientes en el HTMLfunction tratar_datos() {
+function tratar_datos() {
+  if (documentos.length === 0) {
+    alert("Los documentos no se han cargado bien, es posible que el fichero de entrada este vacío")
+  } else {
+    let salida = document.getElementById("salida");
+    salida.innerHTML = "";
+
+    let br = document.createElement("br");
+    let titulo = document.createElement("h3");
+    texto = document.createTextNode("Tabla de los documentos");
+    titulo.appendChild(texto);
+    salida.appendChild(titulo)
+
+    let informacion_documentos = obtener_tablas(documentos)     //  Vector en la que cada posición es un documento, y en cada posicion se almacena un vector con subvectores [indice, termino, tf, idf, tf-idf, tf-normalizados]
+    console.log(informacion_documentos);
+    // Se muestra por pantalla las tablas de cada documento
+    documentos.forEach((documento, n_documento) => {
+      let titulo_documento = document.createElement("h5");
+      texto = document.createTextNode("Documento "+(n_documento+1));
+      titulo_documento.appendChild(texto);
+      salida.appendChild(titulo_documento)
+
+      var container_tabla = document.createElement("div");
+      container_tabla.classList.add('container-md');
+      container_tabla.style.overflowX = "scroll"
+      container_tabla.style.overflowY = "hidden"
+      container_tabla.style.marginBottom = "1%"
+
+      var tabla   = document.createElement("table");
+      tabla.classList.add('table');
+      tabla.classList.add('table-bordered');
+      tabla.classList.add('border-dark');
+      
+      
+      let tblBody = document.createElement("tbody");
+
+      let fila_indices = document.createElement("tr");
+      let celda = document.createElement("th");
+      texto = document.createTextNode("Índices");
+      celda.appendChild(texto);
+      fila_indices.appendChild(celda)
+
+      let fila_terminos = document.createElement("tr");
+      celda = document.createElement("th");
+      texto = document.createTextNode("Términos");
+      celda.appendChild(texto);
+      fila_terminos.appendChild(celda)
+
+      let fila_tf = document.createElement("tr");
+      celda = document.createElement("th");
+      texto = document.createTextNode("TF");
+      celda.appendChild(texto);
+      fila_tf.appendChild(celda)
+
+      let fila_idf = document.createElement("tr");
+      celda = document.createElement("th");
+      texto = document.createTextNode("IDF");
+      celda.appendChild(texto);
+      fila_idf.appendChild(celda)
+
+      let fila_tf_idf = document.createElement("tr");
+      celda = document.createElement("th");
+      texto = document.createTextNode("TF-IDF");
+      celda.appendChild(texto);
+      fila_tf_idf.appendChild(celda)
+
+      informacion_documentos[n_documento].forEach((termino) => {
+        celda = document.createElement("td");
+        texto = document.createTextNode(termino[0]);
+        celda.appendChild(texto);
+        fila_indices.appendChild(celda);
+
+        celda = document.createElement("td");
+        texto = document.createTextNode(termino[1]);
+        celda.appendChild(texto);
+        fila_terminos.appendChild(celda);
+
+        celda = document.createElement("td");
+        texto = document.createTextNode(termino[2]);
+        celda.appendChild(texto);
+        fila_tf.appendChild(celda);
+
+        celda = document.createElement("td");
+        texto = document.createTextNode(termino[3]);
+        celda.appendChild(texto);
+        fila_idf.appendChild(celda);
+
+        celda = document.createElement("td");
+        texto = document.createTextNode(termino[4]);
+        celda.appendChild(texto);
+        fila_tf_idf.appendChild(celda);
+      })
+
+      tblBody.appendChild(fila_indices);
+      tblBody.appendChild(fila_terminos);
+      tblBody.appendChild(fila_tf);
+      tblBody.appendChild(fila_idf);
+      tblBody.appendChild(fila_tf_idf);
+
+      tabla.appendChild(tblBody);
+      container_tabla.appendChild(tabla)
+      salida.appendChild(container_tabla);
+    })
+
+    salida.appendChild(br)
+    salida.appendChild(br)
+
+
+    // Similiaridad coseno
+    let similiaridades = obtener_similiaridades(informacion_documentos)
+
+    // Se muestran por pantallas las similiaridades
+    titulo = document.createElement("h3");
+    texto = document.createTextNode("Similaridad coseno entre cada par de documentos");
+    titulo.appendChild(texto);
+    salida.appendChild(titulo);
+
+    similiaridades.forEach((documento) => {
+      let titulo_documento = document.createElement("h5");
+      texto = document.createTextNode("· Documento "+ (documento[0]+1));
+      titulo_documento.appendChild(texto);
+      salida.appendChild(titulo_documento);
+
+      let comparaciones = document.createElement("p")
+      let comparacionesTexto = "";
+      documento[1].forEach((comparacion) => {
+        comparacionesTexto = comparacionesTexto + "Documento " + (documento[0]+1) + " - " + (comparacion[0]+1) + ": <b>" + comparacion[1] + "</b><br>"
+      })
+
+      comparaciones.innerHTML = comparacionesTexto;
+
+      salida.appendChild(comparaciones);
+    })
+  }  
 }
